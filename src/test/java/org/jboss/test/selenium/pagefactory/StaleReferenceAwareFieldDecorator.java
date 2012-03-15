cf617a1c77f9b05a39d06dff474226ec56d1b9ef
Pavol Pitonak <ppitonak@redhat.com>
2012-03-15 16:30:02 +0100

    StaleReferenceAwareFieldDecorator moved to different package

--- src/test/java/org/jboss/test/selenium/pagefactory/StaleReferenceAwareFieldDecorator.java
+++ src/test/java/org/jboss/test/selenium/pagefactory/StaleReferenceAwareFieldDecorator.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *******************************************************************************/
-package org.jboss.test.selenium.android.support.pagefactory;
+package org.jboss.test.selenium.pagefactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
